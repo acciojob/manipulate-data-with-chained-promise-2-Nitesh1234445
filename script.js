8faf9570a3e04c62c1f0d40ba672ef8b7f939a78
Nitesh Dhakad <110675128+Nitesh1234445@users.noreply.github.com>
2025-11-19 14:36:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+let para = document.getElementById('output');
+
+const promise1 = () => {
+	return new Promise((res) =>{
+		setTimeout(()=>{
+			res([1,2,3,4]);
+		},3000);
+	});
+};
+
+promise1().then((arr) => {
+	let evenArr = arr.filter((ele) => {
+		return (ele % 2 === 0);
+	});
+	
+	setTimeout(()=>{
+		para.textContent = evenArr;
+	},1000);
+	return evenArr;
+}).then((arr) => {
+	let doubleArr = arr.map((ele) => {
+		return 2*ele;
+	});
+
+	setTimeout(() => {
+		para.textContent = doubleArr;
+	},3000);
+});
+
+
+
